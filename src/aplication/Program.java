da40c3bcf23faaef8a5a632dc23174d20e3f16f2
Daniel <danieljunior1903@gmail.com>
2023-09-19 07:10:42 -0400

    Atualização

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -26,7 +26,7 @@ public class Program {
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
         
-        System.out.println("\n=== TEST 1: seller findByDepartment ===");
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2,null);
         List<Seller> list = sellerDao.findByDepartment(department);
         for(Seller obj : list){
