67911989af07617aa20445172795ff3d4bcf2a56
Daniel <danieljunior1903@gmail.com>
2023-09-19 13:31:58 -0400

    Atualização

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,15 +6,44 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.util.List;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 
 /**
  *
  * @author djunior
  */
-public class DepartmentDaoJDBC {
+public class DepartmentDaoJDBC implements DepartmentDao {
     
     private Connection conn;
     public DepartmentDaoJDBC(Connection conn){
         this.conn = conn;
     }    
+
+    @Override
+    public void insert(Department obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Department obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Department fingById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Department> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
